656f78e4cd1fc1de5b7fcbc6fb56a0b030bffaad
momilette <charlotte@anros.fr>
2024-01-15 14:18:20 +0100

    ajout du "expect" pour  le test views/Bills

--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -20,7 +20,6 @@ const row = bill => {
 };
 
 const rows = data => {
-    console.log(data);
     return data && data.length
         ? data
               .sort((a, b) => (a.date < b.date ? 1 : -1))
